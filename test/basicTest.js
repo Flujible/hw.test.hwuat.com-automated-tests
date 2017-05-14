a06edfcfc97f576705bad3bec254d1d69253dc0b
George Bryant <george.s.bryant@gmail.com>
2017-05-14 13:42:42 +0100

    Create first test

--- test/basicTest.js
+++
@@ -1,8 +0,0 @@
-this.basicTest = (browser) => {
-    browser.url('http://hw.test.hwuat.com/pages/homepage.html');
-    browser.waitForElementVisible('body');
-
-
-    browser.assert.title('Test Page Title');
-    browser.end();
-};
---
+++ test/errorMessagesAppearOnBlankSubmit.js
@@ -0,0 +1,17 @@
+this.basicTest = (browser) => {
+  //Navigate to the page and wait until it has loaded
+  browser.url('http://hw.test.hwuat.com/pages/homepage.html');
+  browser.waitForElementVisible('body');
+
+  //Submit the form without inputting any values
+  browser.click("button.btn");
+
+  //Assert that 6 elments with the .errorMessage class are present
+  let numberOfFields = 6;
+  browser.elements("css selector", '.errorMessage', (elements) => {
+    // set the variable to be that collection's length
+    elementCount = elements.value.length;
+    browser.assert.equal(elementCount, numberOfFields);
+  });
+  browser.end();
+};
