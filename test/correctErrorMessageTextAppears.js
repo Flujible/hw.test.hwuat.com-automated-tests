06651d55c2de4ce93c0ab0f76e93f5e30a0db1c0
George <george.bryant@heathwallace.com>
2017-10-09 16:04:58 +0100

    Added a library to communicate passes/failures to saucelabs

--- test/correctErrorMessageTextAppears.js
+++ test/correctErrorMessageTextAppears.js
@@ -1,25 +1,27 @@
-this.correctErrorMessageTextAppears = (browser) => {
-  //Navigate to the page and wait until it has loaded
-  browser
-    .url('http://hw.test.hwuat.com/pages/homepage.html')
-    .waitForElementVisible('body')
-
-  //Submit the form without inputting any values
-    .click("button.btn")
-
-  //Assert that the expected error message appears beneath each input field
-  //I'm not sure why the final the nth child is skipping '2' and jumping straight from 1 to 3
-    .verify.containsText("form#contactForm div div div p.errorMessage", "Please enter your first name")
-    .verify.containsText("form#contactForm div div:nth-child(2) div p.errorMessage", "Please enter your last name")
-    .verify.containsText("form#contactForm div:nth-child(3) div div p.errorMessage", "Please enter your job title")
-    .verify.containsText("form#contactForm div:nth-child(3) div:nth-child(2) div p.errorMessage", "Please enter your phone number")
-    .verify.containsText("form#contactForm div:nth-child(4) div div p.errorMessage", "Please enter your email")
-    .verify.containsText("form#contactForm div:nth-child(4) div:nth-child(2) div p.errorMessage", "Please enter your email")
-
-    .end();
-};
-
-//Uncomment this to disable this test
+const sauce = require("../lib/sauce.js");
 module.exports = {
-  disabled: true
+  "correctErrorMessageTextAppears": (browser) => {
+    //Navigate to the page and wait until it has loaded
+    browser
+      .url('http://hw.test.hwuat.com/pages/homepage.html')
+      .waitForElementVisible('body')
+
+      //Submit the form without inputting any values
+      .click("button.btn")
+
+      //Assert that the expected error message appears beneath each input field
+      //I'm not sure why the final the nth child is skipping '2' and jumping straight from 1 to 3
+      .verify.containsText("form#contactForm div div div p.errorMessage", "Please enter your first name")
+      .verify.containsText("form#contactForm div div:nth-child(2) div p.errorMessage", "Please enter your last name")
+      .verify.containsText("form#contactForm div:nth-child(3) div div p.errorMessage", "Please enter your job title")
+      .verify.containsText("form#contactForm div:nth-child(3) div:nth-child(2) div p.errorMessage", "Please enter your phone number")
+      .verify.containsText("form#contactForm div:nth-child(4) div div p.errorMessage", "Please enter your email")
+      .verify.containsText("form#contactForm div:nth-child(4) div:nth-child(2) div p.errorMessage", "Please enter your email")
+
+      .end();
+  },
+
+  tearDown: sauce
+  //Uncomment this to disable this test
+  // disabled: true
 };
--- test/submitCorrectlyFilledInForm.js
+++ test/submitCorrectlyFilledInForm.js
@@ -1,32 +1,33 @@
-let https = require('https');
+const sauce = require("../lib/sauce.js");
+module.exports = {
+  "submitCorrectlyFilledInForm": (browser) => {
+    //Navigate to the page and wait until it has loaded
+    browser
+      .url('http://hw.test.hwuat.com/pages/homepage.html')
+      // .waitForElementVisible('body')
 
-this.submitCorrectlyFilledInForm = (browser) => {
-  //Navigate to the page and wait until it has loaded
-  browser
-    .url('http://hw.test.hwuat.com/pages/homepage.html')
-    // .waitForElementVisible('body')
+      //Fill the form will all valid inputs including radio button selection
+      .setValue("form#contactForm div div div input.input", "George") //First name
+      .setValue("form#contactForm div div:nth-child(2) div input.input", "Bryant") //Last name
+      .setValue("form#contactForm div:nth-child(3) div div input.input", "QA") //Job title
+      .setValue("form#contactForm div:nth-child(3) div:nth-child(2) div input.input", "07432559576") //Phone number
+      .setValue("form#contactForm div:nth-child(4) div div input.input", "testEmail@testDomain.com") //Email
+      .setValue("form#contactForm div:nth-child(4) div:nth-child(2) div input.input", "testEmail@testDomain.com") //Confirm email
+      .click("input#Radio1.input")
 
-  //Fill the form will all valid inputs including radio button selection
-    .setValue("form#contactForm div div div input.input", "George") //First name
-    .setValue("form#contactForm div div:nth-child(2) div input.input", "Bryant") //Last name
-    .setValue("form#contactForm div:nth-child(3) div div input.input", "QA") //Job title
-    .setValue("form#contactForm div:nth-child(3) div:nth-child(2) div input.input", "07432559576") //Phone number
-    .setValue("form#contactForm div:nth-child(4) div div input.input", "testEmail@testDomain.com") //Email
-    .setValue("form#contactForm div:nth-child(4) div:nth-child(2) div input.input", "testEmail@testDomain.com") //Confirm email
-    .click("input#Radio1.input")
+      //Submit the form
+      .click("button.btn")
 
-  //Submit the form
-    .click("button.btn")
+      //Assert that the URL of the page redirected to contains all the values submitted in the test
+      //Radio inputs have no 'value' attribute, so assuming the value should read 'Yes' to communicate which option was chosen
+      .waitForElementVisible("body")
+      .assert.urlContains("http://hw.test.hwuat.com")
 
-  //Assert that the URL of the page redirected to contains all the values submitted in the test
-  //Radio inputs have no 'value' attribute, so assuming the value should read 'Yes' to communicate which option was chosen
-    .waitForElementVisible("body")
-    .assert.urlContains("http://hw.test.hwuat.com")
+      .end();
+    },
 
-    .end();
+    tearDown: sauce
+
+    //Uncomment this to disable this test
+    // disabled: true
 };
-
-//Uncomment this to disable this test
-// module.exports = {
-//   disabled: true
-// };
